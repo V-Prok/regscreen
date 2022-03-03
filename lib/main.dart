234bbacb417de7a2f6b11c186c3dc766af49a26a
V-Prok <prokopovve@gmail.com>
2022-03-04 01:37:18 +0700

    Реализован вызов информации по http

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:regscreen/2_4_ThemeData.dart';
 import 'package:regscreen/2_5_ListView.dart';
 import 'package:regscreen/3_1_sharedpref.dart';
 import 'package:regscreen/RegScreen2.dart';
+import 'package:regscreen/network.dart';
 import 'package:regscreen/shared_file.dart';
 import 'package:regscreen/authscreen.dart';
 import 'package:regscreen/file_demo.dart';
 
 void main() {
-  runApp(const FileDemo1());
+  runApp(const MyApp());
 }
